ab1ac9217c1dd149df39d536508e0014af0bc2ae
Goo Ninja <karanithiaine4@gmail.com>
2024-03-10 14:51:44 +0300

    Type conversion

--- script.js
+++
@@ -1,5 +0,0 @@
-/* 
-DataType conversion_converting a data type from one typr to another
-              (strings,numbers,booleans)
-              
-*/
---
+++ typeconversion.js
@@ -0,0 +1,51 @@
+/* 
+DataType conversion_converting a data type from one typr to another
+              (strings,numbers,booleans)
+
+*/
+/* 
+let age = window.prompt('How old are you?'); // this is to ask the user how old is he/she?
+age = Number(age);
+age++;
+console.log(age,typeof age);
+ */
+/* 
+let x = 'pizza';
+let y = 'pizza';
+let z = 0;
+x = Number(x);
+y = String(y); 
+z =Boolean(z);//0 returns false,//blank returns false_if the 7user sint type anthing
+
+
+
+
+console.log(x,typeof x);
+
+console.log(y,typeof y);
+
+console.log(z,typeof z);
+
+*/
+
+/* 
+let x;
+let y;
+let z;
+x = Number(x);
+y = String(y); 
+z =Boolean(z);//0 returns false,//blank returns false_if the 7user sint type anthing
+
+
+
+
+console.log(x,typeof x);
+
+console.log(y,typeof y);
+
+console.log(z,typeof z);
+output below
+NaN 'number'
+ undefined string
+false 'boolean'
+ */
